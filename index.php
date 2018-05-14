e8a4a48425fe9130d0cc3a6f05fd70bf9275322a
gzdayou <gz_dayou@hotmail.com>
2018-05-14 16:38:44 +0800

    del

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$site_url = strtolower('http://'.$_SERVER['HTTP_HOST'].substr($_SERVER['PHP_SELF'], 0, strrpos($_SERVER['PHP_SELF'], '/index.php')).'/frontend/');
-@header('Location: '.$site_url);
