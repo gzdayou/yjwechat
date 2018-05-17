1241d3a28785797b0550830e6ee226cd70192185
gzdayou <gz_dayou@hotmail.com>
2018-05-17 11:11:00 +0800

    tab

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -7,6 +7,13 @@ class Index extends Admin
 {
     public function index()
     {
+        $arr = $tab_data = [];
+        $arr['title'] = "后台首页";
+        $arr['url'] = 'admin/index/index';
+        $tab_data['menu'][] = $arr;
+        $tab_data['current'] = 'admin/index/index';
+
+        $this->assign('tab_data', $tab_data);
         return $this->fetch();
     }
 
