36b67ee40d8bc53a8d9c6dfd03c728c7ec1748ed
gzdayou <gz_dayou@hotmail.com>
2018-05-17 15:32:53 +0800

    清楚缓存

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,7 +1,7 @@
 <?php
 namespace app\admin\controller;
 
-use app\admin\model\AdminMenu;
+use app\common\util\Dir;
 
 class Index extends Admin
 {
@@ -17,10 +17,38 @@ class Index extends Admin
         return $this->fetch();
     }
 
-    public function setting() 
+    /**
+     * 清理缓存
+     * @return mixed
+     */
+    public function clear()
     {
-        $menu = AdminMenu::getMainMenu() ;
-        var_dump($menu);exit;
-        return $this->fetch();
+        if ($this->_clear() === false) {
+            return $this->error('缓存清理失败！');
+        }
+        return $this->success('缓存清理成功！');
+    }
+
+    /**
+     * 删除目录（包括下面的文件）
+     * @return void
+     */
+    private function _clear( $directory = RUNTIME_PATH, $subdir = true ) 
+    {
+        if (is_dir($directory) == false) {
+            return false;
+        }
+        $handle = opendir($directory);
+        while (($file = readdir($handle)) !== false) {
+            if ($file != "." && $file != "..") {
+                is_dir("$directory/$file") ?
+                                $this->_clear("$directory/$file") :
+                                @unlink("$directory/$file");
+            }
+        }
+        if (readdir($handle) == false) {
+            closedir($handle);
+            rmdir($directory);
+        }
     }
 }
